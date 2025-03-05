54b66bca47da106aa97dca9b610d6519fa703c51
Henrique Tison <123987816+henriqTSn@users.noreply.github.com>
2025-03-05 11:45:24 -0300

    Add files via upload

---
+++ ExerciciosPraticos4/Class4.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class4
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite uma palavra: ");
+        string? input = Console.ReadLine();
+
+        int quantidadeDeInput = input.Length;
+        Console.WriteLine("A quantidade de caracteres é: " + quantidadeDeInput);
+    }
+}
