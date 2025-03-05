80ed24ad03c2ddab996ef3bd4578e46f21f20fce
Henrique Tison <123987816+henriqTSn@users.noreply.github.com>
2025-03-05 11:43:18 -0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+
+public class Teste1
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite o seu nome: ");
+        string? nome = Console.ReadLine();
+        Console.WriteLine("Olá, " + nome + "! Seja muito bem-vindo!");
+    }
+}
---
+++ ExerciciosPraticos2/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class2
+{
+    static void Main() 
+    {
+        Console.WriteLine("what is your first name? ");
+        string? name = Console.ReadLine();
+        Console.WriteLine("what is your last name? ");
+        string? lastName = Console.ReadLine();
+        Console.WriteLine("Your name is " + name + " " + lastName);
+    }
+}
