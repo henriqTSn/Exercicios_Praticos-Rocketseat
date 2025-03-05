2ea8e8997cc00717ed43856240dbade940738adc
Henrique Tison <123987816+henriqTSn@users.noreply.github.com>
2025-03-05 11:46:20 -0300

    Add files via upload

---
+++ ExerciciosPraticos5/Class5.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Class5
+{
+    static void Main()
+    {
+        Console.WriteLine("Digite a placa do seu carro, EX: abc1234 :");
+        String? placa = Console.ReadLine();
+
+        bool placaValida = VerificarPlaca(placa);
+        Console.WriteLine(placaValida ? "Verdadeiro" : "Falso");
+    }
+    static bool VerificarPlaca(string placa) 
+    {
+        if (placa.Length != 7)
+        {
+            return false;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            //Verifica se é uma letra
+            if (!char.IsLetter(placa[i])) 
+            {
+                return false;
+            }
+        }
+        for (int i = 3; i < 7; i++)
+        {
+            //IsDigit verifica se o caractere for um dígito (0-9)
+            if (!char.IsDigit(placa[i]))
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
